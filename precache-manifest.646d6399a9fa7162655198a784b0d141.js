3e34cc61acefffee26d778b0e08afba6aa00a99c
danielpruitt <35474097+danielpruitt@users.noreply.github.com>
2019-01-01 18:42:57 -0500

    Updates

---
+++ precache-manifest.646d6399a9fa7162655198a784b0d141.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f94a322b571038463e17",
+    "url": "/TinRoof/static/js/runtime~main.f94a322b.js"
+  },
+  {
+    "revision": "9a0ddee7372ab68e18b8",
+    "url": "/TinRoof/static/js/main.9a0ddee7.chunk.js"
+  },
+  {
+    "revision": "e1938df28bf571865bd5",
+    "url": "/TinRoof/static/js/1.e1938df2.chunk.js"
+  },
+  {
+    "revision": "9a0ddee7372ab68e18b8",
+    "url": "/TinRoof/static/css/main.ad8ec073.chunk.css"
+  },
+  {
+    "revision": "9dd34b9da53378dd744478e6e1d64d42",
+    "url": "/TinRoof/index.html"
+  }
+];
